414a90fad98cf005d21cacbd12240e2114e3949b
rasmi <rasminoel@gmail.com>
2023-11-23 10:55:15 +0300

    Done With Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+    print(sequence)
+        
+        
